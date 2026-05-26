581640dd7023c99fbbd0e45212a3369d1e9208b5
paulmrll <paul.morille11@gmail.com>
2026-05-26 14:38:58 +0200

    update

---
+++ SmartFarm/target/Javadoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"org.example","l":"Coordinates"},{"p":"org.example","l":"Main"},{"p":"org.example","l":"Person"},{"p":"org.example","l":"Point"}];updateSearchResults();
